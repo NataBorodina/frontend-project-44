649b1f5815a0d3c7a9bcd5e3173a09b80258bfa1
NataliaBorodina <borodinn@list.ru>
2023-02-10 08:30:53 +0600

    edited progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,7 +5,7 @@ const description = 'What number is missing in the progression?';
 
 const takeArray = (start, step) => {
   const arr = [start];
-  for (let i = 1; arr.length <= 10; i += 1) {
+  for (let i = 1; arr.length <= 9; i += 1) {
     const item = arr[i - 1] + step;
     arr.push(item);
   }
@@ -15,14 +15,14 @@ const takeArray = (start, step) => {
 const takeGameArray = (arr, spaseIndex) => {
   const gameArray = arr;
   gameArray[spaseIndex] = '..';
-  return gameArray;
+  return gameArray.join(' ');
 };
 
 const game = () => {
   const start = getRandomNum(100);
   const step = getRandomNum(100);
   const array = takeArray(start, step);
-  const spaseIndex = Math.floor(Math.random() * 10) + 1;
+  const spaseIndex = Math.floor(Math.random() * 9) + 1;
   const correctAnswer = String(array[spaseIndex]);
   const gameArray = takeGameArray(array, spaseIndex);
   console.log(`Question: ${gameArray}`);
